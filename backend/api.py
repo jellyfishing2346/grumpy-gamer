5d310c984096f3dfaf3adae1f37afa167a48119e
jellyfishing2346 <108194702+jellyfishing2346@users.noreply.github.com>
2026-02-19 16:07:34 -0500

    Enabled CORS and added the vercel application

--- backend/api.py
+++ backend/api.py
@@ -5,9 +5,26 @@ FastAPI app definition and router registration.
 from fastapi import FastAPI
 from .auth import auth_router
 from .chatbot import chatbot_router
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
+
+import os
+ENV = os.environ.get("ENV", "development")
+if ENV == "production":
+    origins = ["https://grumpy-gamer.vercel.app"]
+else:
+    origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router, prefix="/api")
 app.include_router(chatbot_router, prefix="/api")
 
