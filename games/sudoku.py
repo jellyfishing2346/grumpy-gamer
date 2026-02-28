568ab76a8f551ee08d447eea726fd66ff6bb4522
jellyfishing2346 <faizanakhan2003@gmail.com>
2026-02-27 23:51:52 -0500

    Upgrade SudokuAgent with constraint propagation and MRV heuristic

--- games/sudoku.py
+++ games/sudoku.py
@@ -3,9 +3,16 @@ class Sudoku:
         self.board = board
 
     def print_board(self):
-        for row in self.board:
-            print(" ".join(str(num) if num != 0 else '.' for num in row))
-    
+        for i, row in enumerate(self.board):
+            if i % 3 == 0 and i != 0:
+                print("------+-------+------")
+            row_str = ""
+            for j, num in enumerate(row):
+                if j % 3 == 0 and j != 0:
+                    row_str += "| "
+                row_str += (str(num) if num != 0 else '.') + " "
+            print(row_str)
+
     def is_valid(self, row, col, num):
         for i in range(9):
             if self.board[row][i] == num or self.board[i][col] == num:
@@ -16,4 +23,24 @@ class Sudoku:
                 if self.board[start_row + i][start_col + j] == num:
                     return False
         return True
-    
+
+    def get_candidates(self, row, col):
+        """Return the set of valid numbers for a given empty cell."""
+        if self.board[row][col] != 0:
+            return set()
+        return {num for num in range(1, 10) if self.is_valid(row, col, num)}
+
+    def count_empty_cells(self):
+        return sum(1 for row in self.board for cell in row if cell == 0)
+
+    def difficulty(self):
+        """Estimate difficulty based on number of empty cells."""
+        empty = self.count_empty_cells()
+        if empty <= 30:
+            return "Easy"
+        elif empty <= 45:
+            return "Medium"
+        elif empty <= 55:
+            return "Hard"
+        else:
+            return "Expert"
