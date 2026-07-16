5ca36f6ca74e438f80e00a3af7ba1d8e74d99f6b
Faizan Khan <108194702+jellyfishing2346@users.noreply.github.com>
2026-07-16 12:34:41 -0400

    Add financial dashboard for coin transactions (#221) (#236)
    * Add financial dashboard for coin transactions (#221)
    
    * Correcting frontend component: typescript errors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -45,6 +45,7 @@ const AIMetrics = lazy(() => import("./components/pages/AIMetrics"));
 const MultiplayerLobby = lazy(() => import("./components/pages/MultiplayerLobby"));
 const Store = lazy(() => import("./components/pages/Store"));
 const Tournaments = lazy(() => import("./components/pages/Tournaments"));
+const FinancialDashboard = lazy(() => import("./components/pages/FinancialDashboard"));
 
 
 // Component to update the page title on route change
@@ -111,6 +112,7 @@ function App() {
               <Route path="multiplayer" element={<MultiplayerLobby />} />
               <Route path="store" element={<Store />} />
               <Route path="tournaments" element={<Tournaments />} />
+              <Route path="finances" element={<FinancialDashboard />} />
                     <Route path="settings" element={<Settings />} />
                     <Route path="play/wordle" element={<WordleGame />} />
                     <Route path="play/sudoku" element={<SudokuGame />} />
